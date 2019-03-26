eaef3805ba2d3da95bea07702f3a968ff5802357
Jason Bamford <47010074+jb-23@users.noreply.github.com>
2019-03-26 12:52:51 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,22 @@
 def square_array(array)
   # your code here
+
+  f = []
+
+  array.each do |x|
+    f << x ** 2
+  end
+
+  return f
+
+end
+
+
+# collect creates a new array as it goes
+def square_collect(array)
+
+  return array.collect do |x|
+    x ** 2
+  end
+
 end
